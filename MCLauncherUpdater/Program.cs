44079f0df178467db5ed488d11c4d08d26b53264
DEJVOSS <63589160+IronBeagle2@users.noreply.github.com>
2022-07-08 20:51:36 +0200

    Updater 2.0

--- MCLauncherUpdater/Program.cs
+++ MCLauncherUpdater/Program.cs
@@ -11,46 +11,63 @@ namespace MCLauncherUpdater
 {
     class Program
     {
+        public static string currentPath = Directory.GetCurrentDirectory();
+        public static string updatePath = $"{currentPath}\\.codexipsa\\update.cfg";
+
+        public static string updateUrl;
+
         static void Main(string[] args)
         {
-            Console.Title = "MineC#raft Launcher Dev updater v1.2";
-            Console.WriteLine("MineC#raft Launcher Dev updater v1.2");
-            Console.WriteLine("------------------------");
-            try
+            Console.Title = "MineC#raft Launcher updater v2.0";
+            Console.WriteLine("MineC#raft Launcher updater v2.0");
+            Console.WriteLine("--------------------------------");
+            
+            if(File.Exists(updatePath))
             {
-                using (var client = new WebClient())
+                updateUrl = File.ReadAllText(updatePath);
+
+                try
                 {
-                    string currentPath = Directory.GetCurrentDirectory();
-                    client.DownloadFile("http://codex-ipsa.dejvoss.cz/MCL-Data/launcher/launcherDev.zip", currentPath + "\\launcherDev.zip");
+                    using (var client = new WebClient())
+                    {
+                        Console.WriteLine($"[Updater] Url: {updateUrl}");
+                        client.DownloadFile(updateUrl, $"{currentPath}\\launcher.zip");
 
-                    if (File.Exists(currentPath + "\\MCLauncher.exe"))
-                        File.Delete(currentPath + "\\MCLauncher.exe");
-                    if (File.Exists(currentPath + "\\Newtonsoft.Json.dll"))
-                        File.Delete(currentPath + "\\Newtonsoft.Json.dll");
+                        if (File.Exists($"{currentPath}\\MCLauncher.exe"))
+                            File.Delete($"{currentPath}\\MCLauncher.exe");
+                        if (File.Exists($"{currentPath}\\Newtonsoft.Json.dll"))
+                            File.Delete($"{currentPath}\\Newtonsoft.Json.dll");
 
-                    string zipPath = currentPath + "\\launcherDev.zip";
-                    string extractPath = currentPath;
-                    ZipFile.ExtractToDirectory(zipPath, extractPath);
+                        string zipPath = $"{currentPath}\\launcher.zip";
+                        string extractPath = currentPath;
+                        ZipFile.ExtractToDirectory(zipPath, extractPath);
 
-                    File.Delete(currentPath + "\\launcherDev.zip");
-
+                        File.Delete(currentPath + "\\launcher.zip");
+                    }
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine("Update successful!");
+                    Console.ForegroundColor = ConsoleColor.Gray;
+                    Console.WriteLine("Press enter to close this window and run the launcher.");
+                    Console.ReadKey();
+                    System.Diagnostics.Process.Start("MCLauncher.exe");
                 }
-                Console.ForegroundColor = ConsoleColor.Green;
-                Console.WriteLine("Update successful!");
-                Console.ForegroundColor = ConsoleColor.Gray;
-                Console.WriteLine("Press enter to close this window and run the launcher.");
-                Console.ReadKey();
-                System.Diagnostics.Process.Start("CMD.exe", $"/C MCLauncher.exe");
+                catch (WebException)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("A web exception error has occured! (Error code 1)");
+                    Console.WriteLine("Please connect to the internet and try again.");
+                    Console.ForegroundColor = ConsoleColor.Gray;
+                    Console.WriteLine("Press enter to close this window.");
+                    Console.ReadKey();
+                }
             }
-            catch(WebException)
+            else
             {
                 Console.ForegroundColor = ConsoleColor.Red;
-                Console.WriteLine("A web exception error has occured! (Error code 1)");
-                Console.WriteLine("Please connect to the internet and try again.");
+                Console.WriteLine($"[Updater] Update.cfg not found! (Error code: 2)");
                 Console.ForegroundColor = ConsoleColor.Gray;
                 Console.WriteLine("Press enter to close this window.");
                 Console.ReadKey();
-
             }
         }
     }
