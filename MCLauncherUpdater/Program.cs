df3ccead343c7ca8140d777438518bc80c3506c6
DEJVOSS <63589160+IronBeagle2@users.noreply.github.com>
2022-10-22 13:07:44 +0200

    Removed instance updater from here

--- MCLauncherUpdater/Program.cs
+++ MCLauncherUpdater/Program.cs
@@ -33,15 +33,7 @@ namespace MCLauncherUpdater
             }
             else
             {
-                if (args[2] == "-updInst")
-                {
-                    updateInstances();
-
-
-                    Console.ReadLine(); //remove this
-                }
-
-                /*if (args[0] == "-url")
+                if (args[0] == "-url")
                 {
                     updateUrl = args[1];
 
@@ -69,51 +61,6 @@ namespace MCLauncherUpdater
                     {
                         logError("A web exception error has occured! (Error code 1)\nPlease connect to the internet and try again.");
                     }
-                }*/
-            }
-        }
-
-        public static void updateInstances()
-        {
-            string[] dirs = Directory.GetDirectories(".codexipsa\\instance\\");
-            foreach (string dir in dirs)
-            {
-                Console.WriteLine(dir);
-                int index = dir.LastIndexOf("\\") + 1;
-                string name = dir.Substring(index, dir.Length - index);
-
-                if (File.Exists($"{dir}\\instance.cfg"))
-                {
-                    string text = File.ReadAllText($"{dir}\\instance.cfg");
-                    Console.WriteLine(text);
-                    if(text.Contains("classroom"))
-                    {
-                        text = text.Replace($"[\n{{", $"[\n{{\n\"name\":\"{name}\",\n\"edition\":\"MinecraftEdu\",");
-                    }
-                    else
-                    {
-                        text = text.Replace($"[\n{{", $"[\n{{\n\"name\":\"{name}\",\n\"edition\":\"Java Edition\",");
-                    }
-                    text = text.Replace("instVer", "version");
-                    text = text.Replace("instType", "type");
-                    text = text.Replace("instUrl", "url");
-                    text = text.Replace("instDir", "directory");
-                    text = text.Replace("instResWidth", "resolutionX");
-                    text = text.Replace("instResHeight", "resolutionY");
-                    text = text.Replace("instRamMin", "ramMin");
-                    text = text.Replace("instRamMax", "ramMax");
-                    text = text.Replace("useCustJava", "useCustomJava");
-                    text = text.Replace("instCustJava", "customJava");
-                    text = text.Replace("useCustJvm", "useJvmArgs");
-                    text = text.Replace("instCustJvm", "jvmArgs");
-                    text = text.Replace("useCustMethod", "useLaunchMethod");
-                    text = text.Replace("instCustMethod", "launchMethod");
-                    text = text.Replace("\"useCustJar\":\"False\",", String.Empty);
-                    text = text.Replace("\"instCustJar\":\"\",", String.Empty);
-                    text = text.Replace("useOfflineMode", "offlineMode");
-                    Console.WriteLine(text);
-                    Console.WriteLine();
-                    //File.WriteAllText("test.txt", text);
                 }
             }
         }
